7b91e9923355a9c845783379069c39bec1fbe457
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2024-01-21 22:27:39 +0100

    Implement calculations

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	var revenue, expenses, taxRate float64
+
+	captureInput("Revenue", &revenue)
+	captureInput("Expenses", &expenses)
+	captureInput("Tax rate", &taxRate)
+
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate)
+	ratio := ebt / profit
+
+	fmt.Printf("Calculated:\n"+
+		"EBT: %g\n"+
+		"Profit: %g\n"+
+		"Ratio: %g\n",
+		ebt, profit, ratio)
+
+}
+
+func captureInput(msg string, p any) {
+	fmt.Print(msg, ": ")
+	_, err := fmt.Scan(p)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
