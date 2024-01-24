ed956714c375c391f4a8a2ea63486ae251275258
Pawel Daniluk <pawel.daniluk@kotalbert.net>
2024-01-24 21:59:16 +0100

    Add part 2 (#1)
    * Handle input errors
    * Implement write to file

--- main.go
+++ main.go
@@ -1,33 +1,51 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
 
-	var revenue, expenses, taxRate float64
+	revenue, errRev := captureFloat("Revenue")
+	if errRev != nil {
+		log.Fatal(errRev)
+	}
 
-	captureInput("Revenue", &revenue)
-	captureInput("Expenses", &expenses)
-	captureInput("Tax rate", &taxRate)
+	expenses, errExp := captureFloat("Expenses")
+	if errExp != nil {
+		log.Fatal(errExp)
+	}
+
+	taxRate, errTax := captureFloat("Tax rate")
+	if errTax != nil {
+		log.Fatal(errTax)
+	}
 
 	taxRate = taxRate / 100
 	ebt := calcEBT(revenue, expenses)
 	profit := calcProfit(ebt, taxRate)
 	ratio := calcRatio(ebt, profit)
 
-	printResults(ebt, profit, ratio)
-
+	report := getResultsReport(ebt, profit, ratio)
+	fmt.Println(report)
+	writeReportToFile(report)
 }
 
-func printResults(ebt float64, profit float64, ratio float64) {
-	fmt.Printf("Calculated:\n"+
-		"EBT: %g\n"+
-		"Profit: %g\n"+
-		"Ratio: %g\n",
-		ebt, profit, ratio)
+func writeReportToFile(report string) {
+	err := os.WriteFile("profit_report.txt", []byte(report), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getResultsReport(ebt float64, profit float64, ratio float64) string {
+	return fmt.Sprintf(`Calculated:
+EBT: %.2f
+Profit: %.2f
+Ratio: %.2f`, ebt, profit, ratio)
 }
 
 func calcRatio(ebt float64, profit float64) float64 {
@@ -42,10 +60,16 @@ func calcEBT(revenue float64, expenses float64) float64 {
 	return revenue - expenses
 }
 
-func captureInput(msg string, p any) {
+func captureFloat(msg string) (float64, error) {
 	fmt.Print(msg, ": ")
-	_, err := fmt.Scan(p)
+	var userInput float64
+	_, err := fmt.Scan(&userInput)
 	if err != nil {
 		log.Fatal(err)
 	}
+	// handle negative input
+	if userInput <= 0 {
+		return -1, errors.New("expecting positive value")
+	}
+	return userInput, nil
 }
